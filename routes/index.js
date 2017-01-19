60b7f4085731034c0acf06225102514b8f468bfa
Fidel Salgado <fidels@stanford.edu>
2017-01-18 17:22:06 -0800

    adding handlebars helper in route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('home', {
+    title: 'Cool Title', 
+    data: [[1,2,3],[4,5,6]],
+    helpers: {
+      list: function(context) {
+        var ret = "<ul>";
+
+        for(var i=0, j=context.length; i<j; i++) {
+          ret = ret + "<li>" + options.fn(context[i]) + "</li>";
+        }
+
+        return ret + "</ul>";
+      }
+    }
+  });
 });
 
 module.exports = router;
