e02eb801c95c791072aa1e5fbcf23856f9a7e49f
Fidel Salgado <fidels@stanford.edu>
2017-01-18 17:20:26 -0800

    adding error handling to routes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,12 +10,10 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -40,7 +37,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('Page does not exist');
 });
 
 module.exports = app;
